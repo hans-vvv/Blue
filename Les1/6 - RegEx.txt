1a40566793260dc4b9239ee783516b1ae3740ea0
hans-vvv <33095823+hans-vvv@users.noreply.github.com>
2021-05-26 14:56:44 +0200

    First commit

---
+++ Les1/6 - RegEx.txt
@@ -0,0 +1,28 @@
+import re
+
+string = 'switchport mode access'
+match = re.search(r'switchport mode (\w+)', string)
+if match:
+    value = format(match.group(1))
+    
+print(value)
+
+class ReSearcher(object):
+    match = None
+
+    def __call__(self, pattern, string):
+        self.match = re.search(pattern, string)
+        return self.match
+
+    def __getattr__(self, name):
+        return getattr(self.match, name)
+        
+match = ReSearcher()
+
+string = 'switchport mode access'
+if match(r'switchport mode (\w+)', string):
+    value = format(match.group(1))
+    
+print(value)
+
+
---
+++ Les1/7 - basic_parser.py
@@ -0,0 +1,82 @@
+import re
+import json
+
+
+class ReSearcher:
+    match = None
+
+    def __call__(self, pattern, string):
+        self.match = re.search(pattern, string)
+        return self.match
+
+    def __getattr__(self, name):
+        return getattr(self.match, name)
+
+
+class Tree(dict):
+    """ Autovivificious dictionary """
+    def __missing__(self, key):
+        value = self[key] = type(self)()
+        return value
+
+    def __str__(self):
+        """ Serialize dictionary to JSON formatted string with indents """
+        return json.dumps(self, indent=4)
+
+
+def basic_parser(configfile):
+
+    tree = Tree()
+    match = ReSearcher()
+    context = ''
+
+    with open(configfile, 'r') as lines:
+        
+        for line in lines:
+
+            if match(r'hostname (.*)', line):
+                hostname = format(match.group(1))
+                tree['hostname'] = hostname
+
+            elif match(r'^interface (.*)', line):
+                context = 'port'
+                portindex = format(match.group(1))
+
+            elif context == 'port':                
+
+                if match(r'^ switchport mode (\w+)', line):
+                    value = format(match.group(1))
+                    tree['port'][portindex]['switchport mode'] = value
+
+                elif match(r'^ description (.*)', line):
+                    value = format(match.group(1))
+                    tree['port'][portindex]['description'] = value
+
+                elif match(r'^ switchport access vlan (\d+)', line):
+                    value = format(match.group(1))
+                    tree['port'][portindex]['switchport access vlan'] = value
+
+                elif match(r'^ switchport trunk allowed vlan (\d+)', line):
+                    value = format(match.group(1))
+                    tree['port'][portindex]['switchport trunk allowed vlan'] = value
+
+                elif match(r'^ !', line):
+                    context = ''
+
+        return tree
+
+
+def main():
+
+    configfiles = ['7 - Switch1-cfg.txt']
+
+    trees = []
+    for configfile in configfiles:
+        trees.append(basic_parser(configfile))
+
+    with open('7 - trees.json', 'w') as js:
+        json.dump(trees, js, indent=4)
+
+
+if __name__ == "__main__":
+    main()
