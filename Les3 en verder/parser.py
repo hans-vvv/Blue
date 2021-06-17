d62c718007ac1b2f11518a90ec15849f650c1cdc
hans-vvv <33095823+hans-vvv@users.noreply.github.com>
2021-06-17 07:57:50 +0200

    Update parser.py

--- Les3 en verder/parser.py
+++ Les3 en verder/parser.py
@@ -5,13 +5,6 @@ from collections import defaultdict
 from scripts_full_blown_parser.ios_xe_parser import ios_xe_parser
 
 
-def get_tree_by_hostname(trees, hostname):
-
-    for tree in trees:
-        if tree['hostname'] == hostname:
-            return tree
-
-
 def main():
 
     current_dir = os.getcwd()
@@ -27,31 +20,7 @@ def main():
     os.chdir(current_dir)
     with open('trees.json', 'w') as js:
         json.dump(trees, js, indent=4)
-
-    devicenames = []
-    for tree in trees:
-        devicenames.append(tree['hostname'])
-
-    access_ports = defaultdict(list)
-    for devicename in devicenames:
-        tree = get_tree_by_hostname(trees, devicename)
-        for port, portitems in tree['port'].items():
-            if portitems.get('switchport mode') == 'access':
-                access_ports[devicename].append(port)
-
-    # print(access_ports)
-    for devicename in access_ports:
-        print(devicename)
-##        for port in access_ports[device]:
-##            print(device, port)
-                    
-            
-            
-            
-        
-        
-
-    
+   
 
 if __name__ == "__main__":
     main()
